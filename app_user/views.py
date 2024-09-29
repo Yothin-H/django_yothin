3d55b54fa37244a32a2317c81a74cb5d11e797f4
Yothin  Hinwan <yothin_h@kkumail.com>
2024-09-29 21:05:33 +0700

    change

--- app_user/views.py
+++ app_user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from app_user.forms import RegisterForm
 
 # Create your views here.
+ 
+def registerer(request):
+    form = RegisterForm()
+    context = {'form':form}
+    return render(request,'app_user/register.html',context)
