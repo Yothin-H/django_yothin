d24cb448a0c32c37998a4bfd4238e63f3c42a631
Yothin  Hinwan <yothin_h@kkumail.com>
2024-10-02 21:32:08 +0700

    setting mail

--- django_workshop/settings.py
+++ django_workshop/settings.py
@@ -149,7 +149,15 @@ AUTHENTICATION_BACKENDS=[
 ]
 
 #E-mail
-
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / 'test_inbox'
-PASSWORD_RESET_TIMEOUT = 600
+if DEBUG :
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = BASE_DIR / 'test_inbox'
+    PASSWORD_RESET_TIMEOUT = 120
+else :
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_PORT = os.getenv('EMAIL_PORT')
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    PASSWORD_RESET_TIMEOUT = 600
